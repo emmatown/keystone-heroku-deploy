2674ac17df62228deb0b8d6df2ba7f849e649b35
mitchellhamilton <mitchell@hamil.town>
2019-07-29 12:28:24 +1000

    Try again

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const { Keystone } = require("@keystone-alpha/keystone");
-const { KnexAdapter } = require("./knex");
+const { KnexAdapter: _KnexAdapter } = require("@keystone-alpha/adapter-knex");
 const { Text } = require("@keystone-alpha/fields");
 const { GraphQLApp } = require("@keystone-alpha/app-graphql");
 const { AdminUIApp } = require("@keystone-alpha/app-admin-ui");
 const { StaticApp } = require("@keystone-alpha/app-static");
+
+class KnexAdapter extends _KnexAdapter {
+  async _connect(...args) {
+    let ret = await super._connect(...args);
+    await this.knex.raw(`CREATE SCHEMA IF NOT EXISTS ${this.schemaName}`);
+    return ret;
+  }
+}
 
 const keystone = new Keystone({
   name: "Keystone To-Do List",
