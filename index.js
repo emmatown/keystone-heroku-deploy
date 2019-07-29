5f1748aa8a89515a07541eef2939f34bd2a45ac1
mitchellhamilton <mitchell@hamil.town>
2019-07-29 11:23:56 +1000

    Try stuff

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const keystone = new Keystone({
   adapter: new KnexAdapter()
 });
 
+console.log(process.env.DATABASE_URL);
+
 keystone.connect(process.env.DATABASE_URL);
 
 keystone.createList("Todo", {
